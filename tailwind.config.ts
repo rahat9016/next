98f0456ab38ee469928b80a4dfedb3b8c7f510ab
MAHFUZ ISLAM <77238922+Mahfuzislam1695@users.noreply.github.com>
2024-11-22 19:10:54 +0600

    container add

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,17 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+    },
     extend: {
       colors: {
         primary: "#ffffff",
